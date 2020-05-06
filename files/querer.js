818fb0c90b8fbe8e6c82e7c87d8554a126c584f7
KirillGorbach <kirill.gorbach@gmail.com>
2020-05-07 00:07:50 +0300

    Improved queuer

--- files/querer.js
+++ files/querer.js
@@ -1,29 +1,45 @@
-var connparam = {
-    user: 'tuser',
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'pldb',
-    password: 'admin'
-}
 const { Pool } = require('pg')
-const pool = new Pool({connectionString: process.env.DATABASE_URL})//"postgres://tuser:admin@127.0.0.1:5432/pldb"})//process.env.DATABASE_URL})
+let dburl = process.env.DATABASE_URL
+//пул запросов (иначе - подключение к базе данных
+const pool = new Pool({connectionString: dburl})
+
+//шаблонная функция для краткости
 function query (text, params, callback){
         return pool.query(text, params, (err, res) => {
             callback(err, res)
         })
 }
+/*
 
+    В JavaScript есть такое поятие, как промисы - маленькие асинхронные
+    функции, которые не могут ничего выводить, но по завершению могут вызвать
+    другой код. Вернуть значение - результат промиса нельзя.
+
+    Как правило, запрос к бд делается отдельно для каждого клиента,
+    поэтому создатели библиотеки pg (официальной и единственной, насколько я знаю)
+    сделали все запросы на промисах - это быстро и легковесно.
+    Но в моей программе требуется последующая обработка результатов,
+    поэтому я навесил на промис запросов к бд т.н. "колбэк" - вызав другой
+    функции после исполнения (это нормальная практика).
+
+    Поэтому index.js сначала вызывает loader(), который проверяет наличие необходимой таблицы,
+    колбэком делает запрос на получения пользователей и колбэком вызывает основную
+    функцию (запуск сервера).
+    Запись в бд не требует возвращения результатов, поэтому setter() не вызывает колбэком другие функции.
+    Запись производится в два этапа: удаление старой базы (до последнего чтения сервером бд)
+    и добавления нового элемента. Функция setter() вызывается только из класса playerbaseholder.
+
+    Можно, в последствии, заменить хранение всей базы в оперативной памяти на нормальное общение с бд,
+    но это усложнит структуру кода.
+    В настоящий момент база игроков фиксируется одной json-записью в таблице usrs.
+
+ */
 
 function loader(){
     query('create table if not exists usrs (dt json);', null, (err, res) => {
         if (err){
             console.log(err.stack)
         }
-        load()
-    })
-}
-function load(flag = true) {
-    if (flag) {
         query('select * from usrs;', null, (err, res) => {
             if (err) {
                 console.log(err.stack)
@@ -32,30 +48,18 @@ function load(flag = true) {
             var main = require('./index')
             main.mainfunc(res.rows)
         })
-    }else
-        query('select * from usrs;', null, (err, res) => {
-            if (err) {
-                console.log(err.stack)
-            }
-            //console.log("load: result:", res.rows)
-        })
+    })
 }
-
 function setter(someJSON){
     query('delete from usrs;', (err, res) => {
         if (err){
             console.log(err.stack)
         }
-        //load(false)
-    })
-    set(someJSON)
-}
-function set(someJSON){
-    query('insert into usrs values ($1);', [JSON.stringify(someJSON)], (err, res) => {
-        if (err){
-            console.log(err.stack)
-        }
-        //load(false)
+        query('insert into usrs values ($1);', [JSON.stringify(someJSON)], (err, res) => {
+            if (err){
+                console.log(err.stack)
+            }
+        })
     })
 }
 
